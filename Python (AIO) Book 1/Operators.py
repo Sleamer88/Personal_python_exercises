b8e105b1a254bbe6b439bc03ea1a48593a7140cf
Sleamer88 <robstoica48@gmail.com>
2023-02-03 18:04:59 +0100

    Initial commit

--- Python (AIO) Book 1/Operators.py
+++ Python (AIO) Book 1/Operators.py
@@ -1,9 +1,45 @@
 
 # Booleans
 
-x = True / False
+# x = True / False
 x = True
 x = False
 
 # Arithmetic
 
+"""
++ Addition 1+1=2
+- Subtraction 10-1=9
+* Multiplication 3*5=15
+/ Division 10/5=2
+% Modulus(remainder after division) 11%5=1
+** Exponent 3**2=9
+// Floor Division 11//5=2
+"""
+
+# Comparison
+
+"""
+<  Less than
+<=  Less than or equal to
+>  Greater than
+>=  Greater than or equal to
+==  Equal to
+!=  Not equal to
+is  Object identity
+is not  Negated object identity
+"""
+
+# Boolean
+
+"""
+or  x or y    Either x or y is True
+and  x and y    Both x and y are True
+not  not x    x is not True
+"""
+
+num = 10
+if num > 0:
+    print("Positive number")
+else:
+    print("Negative number")
