cce0ae086ad01e668e09dc9477793e4219baaeb1
Sleamer88 <robstoica48@gmail.com>
2023-02-23 12:41:04 +0100

    Update

---
+++ Python (AIO) Book 1/Understanding What Syntax is and Why It Matters.py
@@ -0,0 +1,37 @@
+"""
+Wrong syntax:
+
+value = variabllename
+10 = x
+
+Correct syntax:
+
+variablename = value
+x = 10
+
+"""
+
+# Different ways of writing first and last names in python
+
+first_name = "Alan"
+last_name = "Simpson"
+print(first_name, last_name)
+
+first_name_2 = "Roberto"; last_name_2 = "Stoica"
+print(first_name_2, last_name_2)
+
+first_name_3 = "Rob"; last_name_3 = "Cav"; print(first_name_3, last_name_3)
+
+FirstName = "God"
+LastName = "Damn"
+print(FirstName, LastName)
+
+x = 10
+if x == 0:
+    print("x is nothing")
+else:
+    print("x is",x)
+print("All done")
+
+# The "==" means is equal to in python and is used to compare a variable to another to see if thye are equal
+
